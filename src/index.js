457f7bdc70b5e0c508fe3bda20eee746c699202a
David-TuX <nguyenxuantung473@gmail.com>
2021-10-13 19:36:26 +0700

    create and add [routes] and [app/controllers]

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const { Router } = require('express');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -16,13 +24,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
